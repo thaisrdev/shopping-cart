1d57551fef5e2815cc32e9eff6699128e1072595
Thais Rosa <tsrosa18@gmail.com>
2022-08-11 18:56:01 -0300

    requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (parametro) => {
+  try {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`;
+  const result = await fetch(url);
+  const data = await result.json();
+  return data;
+  } catch (error) {
+   return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,26 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchProducts é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  it('Executa a função fetchProducts com o argumento "computador" e testa se fetch foi chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  it('Testa se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint correto', async () => {
+    expect.assertions(1);
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(url);
+  });
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {    
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));    
+  });
 });
